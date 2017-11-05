5f8d15d86dc8d1e8c16c03c73aebf7b1cbc15370
xuyang <1794003836@qq.com>
2017-11-05 15:30:16 +0800

    first

---
+++ bind.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Administrator on 2017/8/20.
+ */
+function bind() {
+    if (!Function.prototype.bind) {
+        Function.prototype.bind = function (oThis) {
+            if (typeof this !== "function") {
+                throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+            }
+            var aArgs = Array.prototype.slice.call(arguments, 1),
+                fToBind = this,
+                fNOP = function () {
+                },
+                fBound = function () {
+                    return fToBind.apply(this instanceof fNOP && oThis
+                            ? this
+                            : oThis,
+                        aArgs.concat(Array.prototype.slice.call(arguments)));
+                };
+            fNOP.prototype = this.prototype;
+            fBound.prototype = new fNOP();
+            return fBound;
+        };
+    }
+}
+bind();
